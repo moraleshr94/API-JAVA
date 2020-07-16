18ab0ebad19b529b32ad4bf470e63e0a49c65e75
Rafael Morales <ilikelie90@gmail.com>
2020-07-16 18:52:04 -0500

    Main menu example

--- src/apiprueba/main.java
+++ src/apiprueba/main.java
@@ -3,44 +3,6 @@ package apiprueba;
 import java.util.ArrayList;
 
 public class main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		/*
-		LoadAgreement ag = new LoadAgreement(); 
-		String user = System.getProperty("user.name");
-		String ruta = ("C:\\Users\\"+user+"\\Desktop\\Agreement 16072020.TXT");
-		ag.leerArchivo(ruta);
-		ag.cargarDatos();
-		 /*
-		Agreement agrr = new Agreement(ag.getArchivo().get(0), ag.getArchivo().get(1), ag.getProductos() );
-		
-		System.out.println(agrr.getName());
-		System.out.println(agrr.getSignedBy());
-		System.out.println(agrr.getProducts());
-		*/
-		/*
-		WriteAgreement wr = new WriteAgreement();
-		ArrayList fr = new ArrayList();
-		fr.add("Lol");
-		fr.add("ttt");
-		wr.write("Agreement 17072020", "Gerardo", fr);
-		*/
-		
-		/*
-		ArrayList fr = new ArrayList();
-		fr.add("Lol");
-		fr.add("ttt");
-		
-		Agreement agr = new Agreement("Lorena",fr);
-		
-		createFile(agr);
-		creteAgreementObj("C:\\Users\\Rafael\\Desktop\\Agreement 16072020.txt");
-		*/
-		
-	}
-	
 	public static void  createFile(Agreement agr)
 	{
 		WriteAgreement wa = new WriteAgreement();
@@ -50,8 +12,7 @@ public class main {
 	public static void creteAgreementObj(String route)
 	{
 		LoadAgreement la = new LoadAgreement(); 
-		//String user = System.getProperty("user.name");
-		//String route = ("C:\\Users\\"+user+"\\Desktop\\Agreement 16072020.TXT");
+		
 		la.readFile(route);
 		la.loadData();
 		Agreement agr = new Agreement(la.getFile().get(1), la.getProducts() );
@@ -59,6 +20,23 @@ public class main {
 		System.out.println(agr.getName());
 		System.out.println(agr.getSignedBy());
 		System.out.println(agr.getProducts());
+	}
+
+	public static void main(String[] args) {
+		
+				ArrayList fr = new ArrayList();
+		fr.add("Libro");
+		fr.add("Reloj");
+		fr.add("Mesa");
+		fr.add("Impresora");
+		fr.add("Bocina");
+		
+		Agreement agr = new Agreement("Lorena Corona Lima",fr);
+		
+		createFile(agr);
+		creteAgreementObj("C:\\Users\\Rafael\\Desktop\\Agreement 16072020.txt");
+		
+		
 	}
 
 }
